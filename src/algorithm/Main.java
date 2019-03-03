2b8c06a238c6507fa11e0c40bd57b772a576932d
miya <1833054995@qq.com>
2019-03-03 11:43:01 +0800

    三数之和临时提交

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,6 +1,8 @@
 package algorithm;
 
-import algorithm.leetcode.tencent.arrayandstring.MyAtoi;
+import algorithm.leetcode.tencent.arrayandstring.ThreeSum;
+
+import java.util.List;
 
 /**
  * Created by xuzixu on 2019/2/27.
@@ -11,8 +13,11 @@ public class Main {
 //        int[] num2 = {2};
 //        FindMedianSortedArrays test = new FindMedianSortedArrays();
 //        double result = test.findMedianSortedArrays(num1, num2);
-        String s="9223372036854775808";
-        int result=new MyAtoi().myAtoi(s);
+//        String s="9223372036854775808";
+//        int result=new MyAtoi().myAtoi(s);
+
+        int[] nums = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+        List<List<Integer>> result=new ThreeSum().threeSum(nums);
         System.out.print(result);
     }
 }
